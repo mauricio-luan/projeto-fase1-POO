ba0d2eadb768bd4925f92ec47384f757e4f09003
Mauricio Luan <mauricioluan2023@gmail.com>
2026-03-09 20:51:03 -0300

    feat: finaliza parte um

--- src/entities/relatorio/RelatoriosGerenciais.js
+++ src/entities/relatorio/RelatoriosGerenciais.js
@@ -12,4 +12,22 @@ export default class RelatoriosGerenciais {
   geraRelatorioEntradasESaidas() {}
 
   geraRelatorioClientes() {}
+
+  // 1. Valor total arrecadado por período (e opcionalmente categoria)
+  geraRelatorioArrecadacao(dataInicial, dataFinal, tipoCliente = null) {}
+
+  // 2. Situação de um cliente cadastrado (veículos lá dentro e saldo/débito)
+  geraSituacaoClienteCadastrado(identificador) {} // Pode ser o CPF/CNPJ
+
+  // 3. Registros de estacionamento de um cliente cadastrado em determinado período
+  geraHistoricoClienteCadastrado(identificador, dataInicial, dataFinal) {}
+
+  // 4. Registros de estacionamento de um cliente não cadastrado (Avulso) em determinado período
+  geraHistoricoClienteAvulso(placa, dataInicial, dataFinal) {}
+
+  // 5. Relação dos clientes impedidos de entrar no estacionamento
+  geraRelatorioListaNegra() {}
+
+  // 6. Relação dos 10 clientes mais frequentes do ano
+  geraTop10ClientesFrequentes(ano) {}
 }
